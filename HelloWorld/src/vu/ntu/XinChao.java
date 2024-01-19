6c5096d07bb339ca5723bfcf71a5021fd1ceb6b4
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-01-19 08:14:16 +0700

    Ví dụ đầu tiên
    Chỉ để Demo

---
+++ HelloWorld/src/vu/ntu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package vu.ntu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quản lý mã nguồn với GIT");
+
+	}
+
+}
