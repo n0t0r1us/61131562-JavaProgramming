3be006eea4b36467c5918eadcda18b9cbaeb464d
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-02-22 21:44:32 +0700

    Done

---
+++ LAB1/src/vu/Bai3.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package vu;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Bai3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập độ dài cạnh của khối lập phương: ");
+		double canh = scanner.nextDouble();
+	    double theTich = Math.pow(canh, 3);
+	    System.out.printf("Thể tích của khối lập phương là: %.2f\n", theTich);
+	    scanner.close();
+		// TODO Auto-generated method stub
+
+	}
+
+}
