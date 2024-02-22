69fc828a56bc5d4169942805b79e40b657a822e8
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-02-22 23:24:43 +0700

    Done

---
+++ LAB3/src/vu/Bai2.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package vu;
+
+/**
+ * 
+ */
+public class Bai2 {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%d x %d = %d\n", i, j, i * j);
+            }
+            System.out.println(); 
+		// TODO Auto-generated method stub
+
+		}
+	}
+}
