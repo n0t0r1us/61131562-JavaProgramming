73d6656a2e2d02dee9ffb3047db53be9d6987409
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-02-22 22:46:50 +0700

    Update chức năng giải PTB1

--- LAB2/src/vu/Bai4.java
+++ LAB2/src/vu/Bai4.java
@@ -49,7 +49,27 @@ public class Bai4 {
         System.out.println("Chon 3. Tinh Tien dien");
         System.out.println("Chon 4. End");
         System.out.println("\n");
-
+    }
+    
+    public static void giaiPTB1() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Giai Pt ax + b = 0");
+        System.out.print("Nhập hệ số a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Nhập hệ số b: ");
+        double b = scanner.nextDouble();
+        
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phương trình có vô số nghiệm.");
+            } else {
+                System.out.println("Phương trình vô nghiệm.");
+            }
+        } else {
+            double x = -b / a;
+            System.out.printf("Nghiệm của phương trình: x = %.2f\n", x);
+        }
+    
     
     
 
