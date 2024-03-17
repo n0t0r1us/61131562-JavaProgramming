782279a6716c8d56ad3c35a0d01c0ec79e8389ad
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-17 07:14:04 +0700

    Update SVIT
    gồm các thuộc tính điểm java, html, css. Ghi đè phương thức
    getDiem() để tính điểm cho sinh viên IT theo công thức (2*java + html +
    css)/4

--- LAB7/src/vu/Bai3_SinhVienIT.java
+++ LAB7/src/vu/Bai3_SinhVienIT.java
@@ -1,10 +1,19 @@
 package vu;
 
-public class Bai3_SinhVienIT {
+public class Bai3_SinhVienIT extends Bai3_SinhVienPoly{
+	protected double diemJava;
+    protected double diemCss;
+    protected double diemHtml;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public Bai3_SinhVienIT(String hoTen, String nganh, double diemJava, double diemCss, double diemHtml) {
+        super(hoTen, nganh);
+        this.diemJava = diemJava;
+        this.diemCss = diemCss;
+        this.diemHtml = diemHtml;
+    }
 
-	}
-
+    @Override
+    double getDiem() {
+        return (2 * diemJava + diemHtml + diemCss) / 4;
+    }
 }
