454f8e2aebbbfb6f3f534dee1ac260a6ab81f769
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-17 07:45:39 +0700

    Done

--- LAB8/src/vu/Bai1.java
+++ LAB8/src/vu/Bai1.java
@@ -1,10 +1,16 @@
 package vu;
 
 public class Bai1 {
+    public static double sum(double... nums) {
+        double tong = 0;
+        for (double item : nums) {
+            tong += item;
+        }
+        return tong;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        double sum = sum(5, 6, 3, 6.7);
+        System.out.println("Ket qua: " + sum);
+    }
 }
---
+++ LAB8/src/vu/Bai3.java
@@ -0,0 +1,49 @@
+package vu;
+public class Bai3 {
+    public static double sum(double... nums) {
+        double tong = 0;
+        for (double item : nums) {
+            tong += item;
+        }
+        return tong;
+    }
+
+    public static double min(double... nums) {
+        double min = nums[0];
+        for (double item : nums) {
+            min = Math.min(min, item);
+        }
+        return min;
+    }
+
+    public static double max(double... nums) {
+        double max = nums[0];
+        for (double item : nums) {
+            max = Math.max(max, item);
+        }
+        return max;
+    }
+
+    public static String toUpperFirstChar(String name) {
+        String[] ars = name.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ars.length; i++) {
+            String item = ars[i];
+            if (!item.equals("")) {
+                String ch = item.substring(0, 1).toUpperCase();
+                item = ch + item.substring(1).toLowerCase();
+                sb.append(item).append(" ");
+            }
+        }
+
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String ten = toUpperFirstChar(" nguyen  van  nam");
+        System.out.println(ten);
+
+
+    }
+
+}
