768e5935bc0e1289044df327132f91aca4cba0da
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-17 07:19:08 +0700

    Update Class SVPoly

--- LAB7/src/Bai4/SinhVienPoly.java
+++ LAB7/src/Bai4/SinhVienPoly.java
@@ -1,10 +1,38 @@
 package Bai4;
 
-public class SinhVienPoly {
+public abstract class SinhVienPoly {
+    private String hoTen;
+    private String nganh;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public SinhVienPoly(String hoTen, String nganh) {
+        this.hoTen = hoTen;
+        this.nganh = nganh;
+    }
 
-	}
+    public String getHoTen() {
+        return hoTen;
+    }
 
+    public String getNganh() {
+        return nganh;
+    }
+
+    abstract double getDiem();
+
+    public String getHocLuc() {
+        String hocLuc = null;
+        double diem = getDiem();
+        if (diem >= 9) {
+            hocLuc = "Xuat Xac";
+        } else if (diem >= 7.5) {
+            hocLuc = "Gioi";
+        } else if (diem >= 6.5) {
+            hocLuc = "Kha";
+        } else if (diem >= 5) {
+            hocLuc = "Trung binh";
+        } else {
+            hocLuc = "Yeu";
+        }
+        return hocLuc;
+    }
 }
