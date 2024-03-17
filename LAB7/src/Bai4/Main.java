2d840cd25cad1236c6107fd5d7eb19199c4b2488
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-17 07:23:51 +0700

    Update Main Class

--- LAB7/src/Bai4/Main.java
+++ LAB7/src/Bai4/Main.java
@@ -1,10 +1,8 @@
 package Bai4;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        DanhSachSinhVien dssv = new DanhSachSinhVien();
+        dssv.menu();
+    }
 }
