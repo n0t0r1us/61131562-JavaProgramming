bc36ceb8a2a14a86a6dc501b1f3183ddca107661
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-17 07:19:36 +0700

    Update Class SVIT

--- LAB7/src/Bai4/SinhVienIT.java
+++ LAB7/src/Bai4/SinhVienIT.java
@@ -1,10 +1,19 @@
 package Bai4;
 
-public class SinhVienIT {
+public class SinhVienIT extends SinhVienPoly {
+    protected double diemJava;
+    protected double diemCss;
+    protected double diemHtml;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public SinhVienIT(String hoTen, String nganh, double diemJava, double diemCss, double diemHtml) {
+        super(hoTen, nganh);
+        this.diemJava = diemJava;
+        this.diemCss = diemCss;
+        this.diemHtml = diemHtml;
+    }
 
-	}
-
+    @Override
+    double getDiem() {
+        return (2 * diemJava + diemHtml + diemCss) / 4;
+    }
 }
