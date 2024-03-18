3cf004b3ed06ec98124990333fc8f5fff513332d
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-18 17:24:35 +0700

    Thực hành containers – Hiển thị JWindow

---
+++ THplus_JWindow/src/vu/JWindow.java
@@ -0,0 +1,15 @@
+package vu;
+
+public class JWindow extends javax.swing.JWindow {
+	public JWindow(){
+		setSize(300,200);
+		//Các thuộc tính khác
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new JWindow().setVisible(true);
+
+	}
+
+}
