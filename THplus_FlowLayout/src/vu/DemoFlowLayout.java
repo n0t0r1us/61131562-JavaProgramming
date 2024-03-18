53096b3190d83a0ae383790628fa7cad3c9f2d54
perc1val <90999014+n0t0r1us@users.noreply.github.com>
2024-03-18 17:27:53 +0700

    Thực hành Layout Managers - FlowLayout

---
+++ THplus_FlowLayout/src/vu/DemoFlowLayout.java
@@ -0,0 +1,21 @@
+package vu;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+public class DemoFlowLayout extends JFrame{
+	public DemoFlowLayout()	{
+		setTitle("FlowLayout");
+		setSize(300,250);	
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+ 		setLocationRelativeTo(null);	
+ 		setResizable(false);	
+ 		setLayout(new FlowLayout());
+ 		for (int i = 1;	i<=20;i++)	{
+ 			add(new JButton("Button	"+i));
+ 		}
+ 	}
+	public static void main(String[]args){
+		new DemoFlowLayout().setVisible(true);
+	}
+}
